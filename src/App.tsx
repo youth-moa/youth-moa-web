187a874b46499f92ec89e8bad833a09d3325df23
ez1n <eodrmfdl1004@naver.com>
2024-08-01 21:46:50 +0900

    feat: tanstack query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
