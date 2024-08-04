e5011db10bdd4784defbaa204f730bb99d9574b0
ez1n <eodrmfdl1004@naver.com>
2024-08-04 21:24:04 +0900

    chore: 아이콘 추가

---
+++ src/assets/common/index.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite-plugin-svgr/client" />
+import Logo from "./logo.svg?react";
+import Symbol from "./symbol.svg?react";
+
+export { Logo, Symbol }
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite-plugin-svgr/client" />
+import IcoAlarm from "./alarm.svg?react";
+import IcoDownload from "./download.svg?react";
+import IcoSetting from "./setting.svg?react";
+
+
+export { IcoAlarm, IcoDownload, IcoSetting }
