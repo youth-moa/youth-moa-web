5a9720660d59152f8438c15713c8c70618edbdba
ez1n <eodrmfdl1004@naver.com>
2024-08-04 23:49:55 +0900

    feat: 레이아웃 > 헤더, 푸터 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,25 @@
+import Button from "../components/common/Button";
+
 export default function LoginPage() {
-  return <>Login</>
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1>로그인</h1>
+
+      <section className="flex flex-col w-80">
+        <input type="text" placeholder="아이디를 입력해주세요." />
+        <input type="password" placeholder="비밀번호를 입력해주세요." />
+      </section>
+
+      <section className="flex flex-col gap-2 w-80">
+        <Button label={'로그인'} />
+        <Button label={'회원가입'} type="outlined" />
+      </section>
+
+      <section className="my-4">
+        <button className="text-sm font-semibold text-blue">아이디 찾기</button>
+
+        <button className="text-sm font-semibold text-blue">비밀번호 찾기</button>
+      </section>
+    </div>
+  )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
-import LoginPage from "../pages/Login";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements
+} from "react-router-dom";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>Home</>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <LoginPage />
-    ),
-  },
-])
+import Layout from "../layouts/Layout";
+import LoginPage from "../pages/Login";
+import HomePage from "../pages/Home";
+import ProgramPage from "../pages/Program";
+import NoticePage from "../pages/Notice";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/program" element={<ProgramPage />} />
+      <Route path="/notice" element={<NoticePage />} />
+    </Route>
+  )
+)
