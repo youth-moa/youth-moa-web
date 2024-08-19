7bf76048e683c70648566f3958d056fb3b1d18d9
ezin <78401434+ez1n@users.noreply.github.com>
2024-08-19 11:47:30 +0900

    chore: 사용하지 않는 import문 제거

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
 import { Button } from "../components/common/Button";
 import { Input } from "../components/common/Input";
